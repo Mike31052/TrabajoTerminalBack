885ff77da621527d586a5c95a0b3099f26168ed2
DESKTOP-QRPJC2B\Miguel <miva694@gmail.com>
2024-12-09 22:29:38 -0600

    ultimos cambios

--- src/main/java/com/escom/impuestos/model/ResicoEntity.java
+++ src/main/java/com/escom/impuestos/model/ResicoEntity.java
@@ -21,4 +21,50 @@ public class ResicoEntity {
     @NotNull
     @Column(name = "id_usuario")
     private String idUsuario;
+
+    private String mes;
+
+    private int year;
+
+    private Long cantidadPagar;
+
+    public Long getId_registro() {
+        return id_registro;
+    }
+
+    public void setId_registro(Long id_registro) {
+        this.id_registro = id_registro;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Long getCantidadPagar() {
+        return cantidadPagar;
+    }
+
+    public void setCantidadPagar(Long cantidadPagar) {
+        this.cantidadPagar = cantidadPagar;
+    }
 }
